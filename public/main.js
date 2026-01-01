f90a2c2b2aa2dbac6ca0618e93cc1e643e2e2b51
Paula Burghelea <paulaburghelea.github@gmail.com>
2026-01-01 23:44:56 +0000

    feat: nav styling

--- public/main.js
+++ public/main.js
@@ -1,6 +1,8 @@
 const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
+const menuOpen = document.querySelector(".menu-open");
+const menuClose = document.querySelector(".menu-close");
 
 navToggle.addEventListener("click", () => {
 	navToggle.setAttribute("aria-expanded", !primaryNav.hasAttribute("data-visible"))
@@ -8,4 +10,16 @@ navToggle.addEventListener("click", () => {
 	primaryHeader.toggleAttribute("data-overlay");
 });
 
+const scrollWatcher = document.createElement("div");
 
+scrollWatcher.setAttribute("data-scroll-watcher", "");
+primaryHeader.before(scrollWatcher);
+
+const navObserver = new IntersectionObserver(
+	(entries) => {
+		primaryHeader.classList.toggle("sticking", !entries[0].isIntersecting);
+	},
+	{ rootMargin: "50px 0px 0px 0px" }
+);
+
+navObserver.observe(scrollWatcher);
